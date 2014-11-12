9e06bf5dc3c8b53f7a7f56bc313282bda89e8515
jimmy_somsanith <jsomsanith@voyages-sncf.com>
2014-11-12 16:46:49 +0100

    Init ngMyo service spec

---
+++ test/spec/ngMyo-service-spec.js
@@ -0,0 +1,23 @@
+'use strict';
+
+describe('ngMyo Service', function() {
+    beforeEach(module('ngMyo'));
+
+    describe('.on()', function() {
+        it('should register event', inject(function(Myo) {
+            //given
+
+            //when
+
+            //then
+        }));
+
+        it('should register event with default myo ID', inject(function(Myo) {
+            //given
+
+            //when
+
+            //then
+        }));
+    });
+});
